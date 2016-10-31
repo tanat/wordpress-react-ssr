c15f1e4caf11ecf771fe707b58eff24e502197fa
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2016-11-01 08:55:21 +0900

    add article page design

--- scripts/react/article.js
+++ scripts/react/article.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import moment from 'moment';
 
-export default props => (
-  <div>
-    <h1>{props.title.rendered}</h1>
-    <p>{props.date}</p>
-    <div dangerouslySetInnerHTML={{ __html: props.content.rendered }} />
-  </div>
-);
+export default class ArticlePage extends Component {
+
+  onClickLike() {
+    console.log("like");
+  }
+
+  render() {
+    return (
+      <div id="article">
+        <h1>{this.props.title.rendered}</h1>
+        <button className="like_btton" onClick={() => this.onClickLike()}>お気に入り</button>
+        <p className="article_date">{moment(this.props.date).format('YYYY/MM/DD')}</p>
+        <div className="article_body" dangerouslySetInnerHTML={{ __html: this.props.content.rendered }} />
+      </div>
+    );
+  }
+}
+
