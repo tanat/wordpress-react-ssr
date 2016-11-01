f3d689dca7cf9925e9d196c5c46bc52efb7a8f53
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2016-11-01 23:29:20 +0900

    add favorite button action

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,33 @@
 require('babel-register');
 
-const request = require('request');
+const request = require('superagent');
 const express = require('express');
 const handlebars = require('handlebars');
 const fs = require('fs');
 const React = require('react');
 const renderToString = require('react-dom/server').renderToString;
 
+// TODO ここ直したい
 const ArticlePage = require('./react/article').default;
 const TopPage = require('./react/top').default;
 
 const app = express();
-
 const template = handlebars.compile(fs.readFileSync(`${__dirname}/template.html`, 'utf-8'));
 
 const WP_BASE_URL = `${process.env.WP_URL}/wp-json/wp/v2`;
 
+let favorites = [];
+
+/* Pages */
 app.get('/', (req, res) => {
-  request(`${WP_BASE_URL}/posts`, (error, response, body) => {
+  request.get(`${WP_BASE_URL}/posts`).end((error, response) => {
     if (error) {
       return res.sendStatus(500);
     }
 
-    const data = { articles: JSON.parse(body) };
+    const data = {
+      articles: response.body,
+    };
 
     res.send(template({
       body: renderToString(React.createElement(TopPage, data)),
@@ -32,18 +37,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/[0-9]+', (req, res) => {
-  const permalink = +req.url.match(/\/([0-9]+)/)[1];
+  const id = +req.url.match(/\/([0-9]+)/)[1];
 
-  request(`${WP_BASE_URL}/posts/${permalink}`, (error, response, body) => {
+  request.get(`${WP_BASE_URL}/posts/${id}`).end((error, response) => {
     if (error) {
       return res.sendStatus(500);
     }
 
+    const data = Object.assign({}, response.body, {
+      favorited: !!favorites.filter(f => f === id).length,
+    });
+
     res.send(template({
-      body: renderToString(React.createElement(ArticlePage, JSON.parse(body))),
-      initialData: body,
+      body: renderToString(React.createElement(ArticlePage, data)),
+      initialData: JSON.stringify(data),
     }));
   });
+});
+
+/* APIs */
+app.post('/api/[0-9]+/favorite', (req, res) => {
+  const id = +req.url.match(/\/([0-9]+)/)[1];
+  console.log(`POST /favorite ${id}`);
+
+  favorites.push(id);
+  res.send(200);
+});
+
+app.delete('/api/[0-9]+/favorite', (req, res) => {
+  const id = +req.url.match(/\/([0-9]+)/)[1];
+  console.log(`DELETE /favorite ${id}`);
+
+  favorites = favorites.filter(f => f !== id);
+  res.send(200);
 });
 
 app.listen(process.env.PORT || 3000, () => {
--- scripts/react/article.js
+++ scripts/react/article.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import request from 'superagent';
 
 export default class ArticlePage extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      favorited: props.favorited,
+    };
+  }
+
   onClickLike() {
-    console.log("like");
+    const method = this.state.favorited ? 'del' : 'post';
+
+    request[method](`/api/${this.props.id}/favorite`).end((error) => {
+      if (error) {
+        console.error(error);
+      }
+
+      this.setState({
+        favorited: !this.state.favorited,
+      });
+    });
   }
 
   render() {
+    const { favorited } = this.state;
+
     return (
       <div id="article">
         <h1>{this.props.title.rendered}</h1>
-        <button className="like_btton" onClick={() => this.onClickLike()}>お気に入り</button>
+        <button
+          className={`like_btton${favorited ? ' on' : ''}`}
+          onClick={() => this.onClickLike()}
+        >{ favorited ? 'お気に入り' : 'お気に入りを解除'}</button>
+
         <p className="article_date">{moment(this.props.date).format('YYYY/MM/DD')}</p>
         <div className="article_body" dangerouslySetInnerHTML={{ __html: this.props.content.rendered }} />
       </div>
